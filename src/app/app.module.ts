cd70115be062b5c59fc85aa8625b46dccaca94d1
JassonMendoza <Jassonmendoza1@gmail.com>
2022-04-20 15:57:49 -0400

    created tab search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { TabMainComponent } from './tab-main/tab-main.component';
+import { TabSearchComponent } from './tab-search/tab-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    TabMainComponent
+    TabMainComponent,
+    TabSearchComponent
   ],
   imports: [
     BrowserModule
