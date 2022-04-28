33f31d926bbf0ee4257496cab84a56f72b2038d9
Hendo1349 <hendo1349@gmail.com>
2022-04-27 20:58:22 -0400

    rough draft create-tab component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { LandingComponent } from './landing/landing.component';
 import { TabMainComponent } from './tab-main/tab-main.component';
+import { CreateTabComponent } from './create-tab/create-tab.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    TabMainComponent
+    TabMainComponent,
+    CreateTabComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [],
